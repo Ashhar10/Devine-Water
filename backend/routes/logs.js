8aa4b46f2b54cf21a45cda84e9a0e0babe83bc25
Muhammad Ashhar Alam <m.ashhar10@gmail.com>
2025-12-23 22:29:58 +0500

    Updated

--- backend/routes/logs.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const logController = require('../controllers/logController');
-const { auth, authorize } = require('../middleware/auth');
-
-router.get('/', auth, authorize('admin'), logController.getLogs);
-router.get('/user/:id', auth, authorize('admin'), logController.getUserActivity);
-
-module.exports = router;
